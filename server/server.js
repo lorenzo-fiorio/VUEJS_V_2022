28b49da905a5b7139fce1da06a2147c6cf35bba7
Lorenzo Fiorio <fiorio.lorenzo@outlook.it>
2022-10-14 14:38:52 +0200

    init vue app

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 
 // Utility methods
-function writeDb(){
+function writeDb() {
 	return new Promise(resolve => {
 		let data = JSON.stringify(db, null, 2)
 		fs.writeFile('./db-mock.json', data, (err) => {
@@ -13,27 +13,27 @@ function writeDb(){
 			if (err) throw err;
 			resolve('ok')
 		});
-  });
+	});
 }
 
 // Routes
 
-fastify.get('/api/heroes/:id', async (request, reply) => {
+fastify.get('/api/heroes/:id', async (request) => {
 	if (request.params?.id) {
 		console.log(request.params.id)
 		return db.characters.filter(item => item.id === request.params.id)
-	}else return db.characters
+	} else return db.characters
 })
 
 fastify.put('/api/heroes/:id/xp', async (request, reply) => {
 	try {
 		let character = db.characters.filter(item => item.id === request.params.id)[0]
 		if (request.body) {
-			if(request.body.xp) character.xp += request.body.xp
-			if(request.body.action) character.log.push(request.body.action)
+			if (request.body.xp) character.xp += request.body.xp
+			if (request.body.action) character.log.push(request.body.action)
 		}
-		
-		writeDb().then((response)=> reply.code(201).send())
+
+		writeDb().then(() => reply.code(201).send())
 	} catch (e) {
 		reply.code(500).send('error')
 	}
@@ -41,8 +41,8 @@ fastify.put('/api/heroes/:id/xp', async (request, reply) => {
 
 // Server startup
 fastify.listen({ port: 3000 })
-  .then((address) => console.log(`server listening on ${address}`))
-  .catch(err => {
-    console.log('Error starting server:', err)
-    process.exit(1)
-  })
+	.then((address) => console.log(`server listening on ${address}`))
+	.catch(err => {
+		console.log('Error starting server:', err)
+		process.exit(1)
+	})
---
+++ src/views/LoginView.vue
@@ -0,0 +1,3 @@
+<template>
+    LoginView
+</template>
